f505a463ca5e9f5ad4ca7b268b8d6cd37c638221
izJordy <113905965+izJordy@users.noreply.github.com>
2023-11-28 23:46:33 +0100

    Add files via upload

--- javascript/shoppingCart.js
+++ javascript/shoppingCart.js
@@ -2,10 +2,37 @@ const addToCartButtons = document.getElementsByClassName("product-button");
 const message = document.getElementById("message");
 const cartItems = document.getElementById("cart-items");
 const totalPriceElement = document.getElementById("total-price");
-const remove = document.getElementById("remove");
+const checkoutText = document.getElementById("checkout-text");
 
-const productNames = ["Asus Rog Azoth", "Asus Rog Claymore", "Wooting 60HE"];
-const productPrices = [295.45, 230.87, 189.99];
+const productNames = [
+  "Asus Rog Azoth",
+  "Asus Rog Claymore",
+  "Wooting 60HE",
+  "Keychron Q1",
+  "iQunix ZX75",
+  "Razer Widow V4",
+  "Razer Huntsman V2",
+  "Corsair K100 RGB",
+  "HyperX Alloy Origins",
+  "SteelSeries Apex Pro",
+  "Ducky Shine 7",
+  "Anne Pro"
+];
+
+const productPrices = [
+  295.45,
+  230.87,
+  189.99,
+  210.00,
+  185.07,
+  270.99,
+  150.19,
+  234.90,
+  120.99,
+  210.99,
+  187.88,
+  80.99
+];
 
 let cart = [];
 let totalPrice = 0;
@@ -19,8 +46,8 @@ for (let i = 0; i < addToCartButtons.length; i++) {
   });
 }
 
-remove.addEventListener("click", function(e) {
-  alert("hello");
+checkoutText.addEventListener("click", function(e) {
+  alert("scam successful\n\nsending virus..\npress 'OK' to accept the virus");
 })
 
 //function om toetevoege aan cart[] en gaat ook checken if product already exists in cart[]
@@ -50,6 +77,10 @@ function addToCart(productIndex) {
 
 
 
+
+
+
+
 //nog eens overgaan
 function updateTotalPrice() {
   totalPrice = cart.reduce(function (total, item) {
@@ -62,10 +93,44 @@ function updateTotalPrice() {
 function showCart() {
   cartItems.innerHTML = "";
 
-  cart.forEach(function(item) {
+  cart.forEach(function(item, index) {
     const cartItem = document.createElement("li");
-    cartItem.innerHTML = `<br>${item.name}<br>Quantity: ${item.quantity}<br>€${(item.price * item.quantity).toFixed(2)}`;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "x";
+    deleteButton.classList.add("product-button-remove");
+    deleteButton.addEventListener("click", function () {
+      removeItemFromCart(index);
+    });
+
+    cartItem.innerHTML = `${item.name}<br>Quantity: ${item.quantity}<br>€${(item.price * item.quantity).toFixed(2)}`;
+    cartItem.appendChild(deleteButton);
     cartItems.appendChild(cartItem);
   });
+
+  updateCheckoutText();
+}
+
+function removeItemFromCart(index) {
+  const item = cart[index];
+
+  //als quantity groter dan 1 is dan -1 als het 1 is dan delete element
+  if (item.quantity > 1) {
+    item.quantity--;
+  } 
+  else {
+    cart.splice(index, 1);
+  }
+
+  showCart(); // Update the displayed cart
+  updateTotalPrice(); // Update the total price
+}
+
+function updateCheckoutText() {
+  if (cart.length > 0) {
+    checkoutText.innerHTML = "$Checkout";
+  } else {
+    checkoutText.innerHTML = "";
+  }
 }
 
