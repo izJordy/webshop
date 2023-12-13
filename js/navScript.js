6ebf82d6da99dc5b1d26ee5be2ab53776898c10b
izJordy <113905965+izJordy@users.noreply.github.com>
2023-12-13 20:59:10 +0100

    Add files via upload

--- js/navScript.js
+++ js/navScript.js
@@ -1,7 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector("#nav-menu");
+const searchBar = document.getElementById("search-bar");
+const searchIcon = document.getElementById('search-icon');
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+});
+
+searchIcon.addEventListener("click", () => {
+  searchBar.classList.toggle('show-search-bar');
 });
