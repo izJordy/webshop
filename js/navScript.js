d12da3064a3a330ffcf9d62523b495008624bdfd
izJordy <113905965+izJordy@users.noreply.github.com>
2023-12-15 22:10:47 +0100

    Add files via upload

--- js/navScript.js
+++ js/navScript.js
@@ -1,13 +1,30 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector("#nav-menu");
-const searchBar = document.getElementById("search-bar");
-const searchIcon = document.getElementById('search-icon');
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
 
-searchIcon.addEventListener("click", () => {
-  searchBar.classList.toggle('show-search-bar');
-});
+const searchBar = document.getElementById('search-bar');
+const searchButton = document.getElementById('search-icon');
+
+function toggleSearchBar() {
+  if (searchBar.style.display === 'none' || searchBar.style.display === '') {
+    searchBar.style.display = 'block';
+  } else {
+    searchBar.style.display = 'none';
+  }
+}
+
+searchButton.addEventListener('click', toggleSearchBar);
+
+const submitButton = document.getElementById("submit-button");
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  alert("functionality not added.");
+})
+
+
+
